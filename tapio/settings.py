4d7a51257f07c87f3c68588bafabfe4d5bd8cded
Eloi <e.baudelle@gmail.com>
2023-04-26 23:02:12 +0200

    Update requirements.txt

--- tapio/settings.py
+++ tapio/settings.py
@@ -76,17 +76,16 @@ WSGI_APPLICATION = 'tapio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "tapio",
-#         "USER": "eloi",
-#         "PASSWORD": "tapio",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

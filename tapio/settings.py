d21caffa81ca0fa4242a739c08f950b004a7fe56
Eloi <e.baudelle@gmail.com>
2023-04-28 13:13:52 +0200

    Update settings.py

--- tapio/settings.py
+++ tapio/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 
 ALLOWED_HOSTS=['127.0.0.1', 'tapio-six.vercel.app', 'tapio-caspaaa.vercel.app']
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'https://tapio-front.vercel.app'
